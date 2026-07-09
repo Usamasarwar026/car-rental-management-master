74a6cc7ad88e9aa7f115458a64f9eceb04b9d129
usamasarwar026 <usamasarwar160492@gmail.com>
2026-07-09 11:13:02 +0500

    fix: use relative baseURL for API calls

--- src/store/slices/store.ts
+++ src/store/slices/store.ts
@@ -1,41 +1,4 @@
-// import { configureStore, combineReducers } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import authSlice from "./slices/authSlice";
-// import userDetailsSlice from "./slices/userDetailsSlice";
-// import carCrudSlice from "./slices/carCrudSlice";
-// import eventSlice from "./slices/eventSlice";
-// import { useDispatch, useSelector, useStore } from "react-redux";
 
-// const rootReducer = combineReducers({
-//   authStore: authSlice,
-//   userDetailStore: userDetailsSlice,
-//   carCrudStore: carCrudSlice,
-//   eventStore: eventSlice,
-// });
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: false,
-//     }),
-// });
-
-// export const persistor = persistStore(store);
-
-// export type RootState = ReturnType<typeof rootReducer>;
-// export type AppDispatch = typeof store.dispatch;
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-// export const useAppSelector = useSelector.withTypes<RootState>();
-// export const useAppStore = () => useStore<RootState>();
 
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
